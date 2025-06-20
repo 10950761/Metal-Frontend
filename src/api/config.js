6c12bf03ec8bc925a47c71a97b826d62aa61c08e
10950761 <gtawiah009@st.ug.edu.gh>
2025-06-20 16:55:53 +0000

    changes

--- src/api/config.js
+++ src/api/config.js
@@ -1,4 +1,4 @@
-const API_BASE_URL = process.env.REACT_APP_API_URL || "https://metal-backend-j039.onrender.com";
+const API_BASE_URL =  "https://metal-backend-j039.onrender.com";
 
 
 export default API_BASE_URL;
