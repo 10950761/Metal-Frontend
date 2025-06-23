fe95a9aa7248debfa451e1f44e67e1f99613c7b3
10950761 <gtawiah009@st.ug.edu.gh>
2025-06-23 07:45:39 +0000

    update

--- src/api/config.js
+++ src/api/config.js
@@ -1,5 +1,5 @@
-/* const API_BASE_URL =  "https://metal-backend-j039.onrender.com"; */
-const API_BASE_URL = "http://localhost:5000";
+const API_BASE_URL =  "https://metal-backend-j039.onrender.com"; 
+
 
 
 export default API_BASE_URL;
